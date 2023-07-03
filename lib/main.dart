dd1fd44d815a880bfa2606116cafab9d41a8d2c5
BIPLOB-SHIL <biplobshil.duet@gmail.com>
2023-07-04 00:03:00 +0600

    Third commit

--- lib/main.dart
+++ lib/main.dart
@@ -92,7 +92,7 @@ class _MyHomeState extends State<MyHome> {
                   keyboardType: TextInputType.phone,
                   obscureText: _isVisible,
                   decoration: InputDecoration(
-                      labelText: "Pas",
+                      labelText: "Password",
                       border: const OutlineInputBorder(),
                       prefixIcon: const Icon(Icons.lock_open_outlined),
                       suffixIcon: IconButton(onPressed: ()
